bc7547c7e141f0798cb558962f613cf32af3daac
elzayat3 <abdelrahmanelzayat2025@gmail.com>
2026-03-15 14:22:43 +0200

    fix some logical error in the utils.c

--- src/utils.c
+++ src/utils.c
@@ -1,53 +1,55 @@
+#include "../include/utils.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <windows.h>
-#include "../include/utils.h"
 
-
-void print_string(const char*s)
+void print_string(const char *s)
 {
-	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 11);
-    int i=0;
-    for(; s[i]; i++)
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 14);
+
+    for (int i = 0; s[i]; i++)
     {
-        printf("%c",s[i]);
+        printf("%c", s[i]);
     }
-	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 7);
+
+   SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 7);
     printf("\n");
 }
-void scan_string(char*s)
+
+void scan_string(char *s)
 {
-    int i=0;
-    fflush(stdin);
-    scanf("%c",&s[i]);
-    while(s[i]!='\n')
+    int i = 0;
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 14);
+    scanf(" %c", &s[i]);
+
+    while (s[i] != '\n')
     {
         i++;
-        scanf("%c",&s[i]);
+        scanf("%c", &s[i]);
     }
-    s[i]=0;
+
+    s[i] = 0;
 }
 
-comp_t string_comp(const char* s1, const char* s2)
+comp_t string_comp(const char *s1, const char *s2)
 {
-    comp_t r=same;
-    int i=0;
-    for(; (s1[i]||s2[i])&&(same==r); i++)
+    comp_t r = same;
+
+    for (int i = 0; (s1[i] || s2[i]) && (same == r); i++)
     {
-        if((s1[i])!=(s2[i]))
+        if (s1[i] != s2[i])
         {
-            r=notSame;
+            r = notSame;
         }
-        else {;}
     }
+
     return r;
 }
-void print_arrOfSring(const char** s, int size)
+
+void print_arrOfSring(const char **s, int size)
 {
-    int i;
-    for(i=0;i<size;i++)
+    for (int i = 0; i < size; i++)
     {
         print_string(s[i]);
-        //printf("\n");
     }
 }
